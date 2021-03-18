363a9edbd29e5303c93d53c126254780f1040480
Barry Nguyen <barry.tuan.nguyen@gmail.com>
2021-03-18 14:10:45 +1100

    assignment submitted

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Your code here
+
+let dodger = document.getElementById("dodger");
+let game = document.getElementById("game");
+
+document.addEventListener("keydown", function(e) {
+    console.log(e.key);
+    switch (e.key) {
+
+        case "ArrowLeft":{
+            moveDodgerLeft(); 
+            break;
+        }
+        
+        case "ArrowRight": {
+            moveDodgerRight(); 
+            break; 
+        }
+        case "ArrowUp": {
+            let bottomNumbers = dodger.style.bottom.replace("px", "");
+            let bottom = parseInt(bottomNumbers, 10);
+    
+            dodger.style.bottom = `${bottom + 1}px`;
+            break;
+        }
+        case "ArrowDown":
+            let bottomNumbers = dodger.style.bottom.replace("px", "");
+            let bottom = parseInt(bottomNumbers, 10);
+            dodger.style.bottom = `${bottom - 1}px`;
+            break;
+            
+      }
+  });
+
+  function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+    if (left > 0)
+    dodger.style.left = `${left - 1}px`;
+  }
+
+  function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+    if (left < 360)
+    dodger.style.left = `${left + 1}px`;
+
+  }
+  
